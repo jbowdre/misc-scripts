d3e2b308093db39422605da33c2cdd213a92b1fe
jbowdre <john@bowdre.net>
2021-06-24 13:57:08 -0500

    check for osType in loop in case the VM gets DRS-migrated while the script is running

--- vRealize/configure_guest.ps1
+++ vRealize/configure_guest.ps1
@@ -48,8 +48,22 @@ function handler($context, $inputs) {
     }
     
     # Detect hostname and OS type
-    $osType = ($vm | Get-View).Guest.GuestFamily.ToString()
-    $toolsStatus = ($vm | Get-View).Guest.ToolsStatus.ToString()
+    $count = 0
+    While (!$osType) {
+        Try {
+            $osType = ($vm | Get-View).Guest.GuestFamily.ToString()
+            $toolsStatus = ($vm | Get-View).Guest.ToolsStatus.ToString()        
+        } Catch {
+            # 60s timeout
+            if ($count -ge 12) {
+                Write-Error "Timeout exceeded while waiting for tools." -ErrorAction Stop
+                break
+            }
+            Write-Host "Waiting for tools..."
+            $count++
+            Sleep 5
+        }
+    }
     Write-Host "$vmName is a $osType and its tools status is $toolsStatus."
     # Update tools on Windows if out of date
     if ($osType.Equals("windowsGuest") -And $toolsStatus.Equals("toolsOld")) {
--- vRealize/localAccounts.ps1
+++ vRealize/localAccounts.ps1
@@ -60,9 +60,23 @@ function handler($context, $inputs) {
     if (-not (Wait-Tools -VM $vm -TimeoutSeconds 180)) {
         Write-Error "Unable to establish connection with VM tools" -ErrorAction Stop
     }
-    # Detect OS family (win/lin)
-    $osType = ($vm | Get-View).Guest.GuestFamily.ToString()
-    $toolsStatus = ($vm | Get-View).Guest.ToolsStatus.ToString()
+    # Detect OS type
+    $count = 0
+    While (!$osType) {
+        Try {
+            $osType = ($vm | Get-View).Guest.GuestFamily.ToString()
+            $toolsStatus = ($vm | Get-View).Guest.ToolsStatus.ToString()        
+        } Catch {
+            # 60s timeout
+            if ($count -ge 12) {
+                Write-Error "Timeout exceeded while waiting for tools." -ErrorAction Stop
+                break
+            }
+            Write-Host "Waiting for tools..."
+            $count++
+            Sleep 5
+        }
+    }
     Write-Host "$vmName is a $osType and its tools status is $toolsStatus."
     # Update tools on Windows if out of date
     if ($osType.Equals("windowsGuest") -And $toolsStatus.Equals("toolsOld")) {
